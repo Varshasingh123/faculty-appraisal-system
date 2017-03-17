3293837ca478f7d8419464db3fada58d4f119071
Varshasingh123 <Varshaasinghh7@gmail.com>
2017-03-17 15:17:35 +0530

    Add files via upload

---
+++ CourseDetail.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+/// <summary>
+/// Summary description for CourseDetail
+/// </summary>
+public class CourseDetail
+{
+    String CID, Cc1,Cc2,Cres,log ,err;
+
+	int Cnof,Cterm;
+    decimal CL, CT, CP, CLTP,Ca,Cb,Ccd;
+    public String Login
+    {
+        get
+        {
+            return log;
+        }
+
+        set
+        {
+            log = value;
+        }
+    }
+    public String Error
+    {
+        get
+        {
+            return err;
+        }
+
+        set
+        {
+            err = value;
+        }
+    }
+    public int CourseTerm
+    {
+        get
+        {
+            return Cterm;
+        }
+
+        set
+        {
+            Cterm = value;
+        }
+    }
+    public String CourseID
+    {
+        get
+        {
+            return CID;
+        }
+
+        set
+        {
+            CID = value;
+        }
+    }
+    public decimal CourseL
+    {
+        get
+        {
+            return CL;
+        }
+
+        set
+        {
+            CL = value;
+        }
+    }
+    public decimal CourseT
+    {
+        get
+        {
+            return CT;
+        }
+
+        set
+        {
+            CT = value;
+        }
+    }
+    public decimal CourseP
+    {
+        get
+        {
+            return CP;
+        }
+
+        set
+        {
+            CP = value;
+        }
+    }
+    public decimal CourseLTP
+    {
+        get
+        {
+            return CLTP;
+        }
+
+        set
+        {
+            CLTP = value;
+        }
+    }
+    public int CourseNOF
+    {
+        get
+        {
+            return Cnof;
+        }
+
+        set
+        {
+            Cnof = value;
+        }
+    }
+
+    public decimal CourseA
+    {
+        get
+        {
+            return Ca;
+        }
+
+        set
+        {
+            Ca = value;
+        }
+    }
+    public decimal CourseB
+    {
+        get
+        {
+            return Cb;
+        }
+
+        set
+        {
+            Cb = value;
+        }
+    }
+    public decimal CourseD
+    {
+        get
+        {
+            return Ccd;
+        }
+
+        set
+        {
+            Ccd = value;
+        }
+    }
+    
+    public String CourseCc1
+    {
+        get
+        {
+            return Cc1;
+        }
+
+        set
+        {
+            Cc1 = value;
+        }
+    }
+    public String CourseCc2
+    {
+        get
+        {
+            return Cc2;
+        }
+
+        set
+        {
+            Cc2 = value;
+        }
+    }
+
+    public String CourseRst
+    {
+        get
+        {
+            return Cres;
+        }
+
+        set
+        {
+            Cres = value;
+        }
+    }
+    public static DataTable Courses(int term, String log)
+    {
+        String cns = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Saurabh\Desktop\Appraisal\App_Data\Database.mdf;Integrated Security=True";
+       String cms = "Select * from CourseS where term=" + term + " and login='" + log + "'" ;
+       SqlDataAdapter da=new SqlDataAdapter(cms,cns);
+        DataSet ds=new DataSet();
+        da.Fill(ds);
+        return ds.Tables[0];
+    }
+    public String IsEntered()
+    {
+
+        try
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Saurabh\Desktop\Appraisal\App_Data\Database.mdf;Integrated Security=True";
+            cn.Open();
+            SqlCommand cm = new SqlCommand();
+            cm.CommandText = "select count(*) from CourseS where login=@log and term=@term and CourseID=@cid";
+            cm.Connection = cn;
+            cm.CommandType = CommandType.Text;
+            cm.Parameters.AddWithValue("@log", log);
+            cm.Parameters.AddWithValue("@term", Cterm);
+            cm.Parameters.AddWithValue("@cid", CID);
+
+            int count = (int)cm.ExecuteScalar();
+            if (count > 0)
+            {
+                return "yes";
+            }
+            else
+            {
+                return "no";
+            }
+
+        }
+        catch (Exception e1)
+        {
+            err = e1.Message;
+            return "no";
+        }
+
+    }
+    public bool NewCourseD()
+    {
+        bool res = false;
+        try
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Saurabh\Desktop\Appraisal\App_Data\Database.mdf;Integrated Security=True";
+            cn.Open();
+            SqlCommand cm = new SqlCommand();
+            cm.CommandText = "insert into CourseS values(@log,@ter,@cid,@cl,@ct,@cp,@cltp,@cnof,@ca,@cb,@cc1,@cc2,@cres,@ccd)";
+            cm.Connection = cn;
+            cm.CommandType = CommandType.Text;
+            cm.Parameters.AddWithValue("@log", log);
+            cm.Parameters.AddWithValue("@ter", Cterm);
+            cm.Parameters.AddWithValue("@cid", CID);
+            cm.Parameters.AddWithValue("@cl", CL);
+            cm.Parameters.AddWithValue("@ct", CT);
+            cm.Parameters.AddWithValue("@cp", CP);
+            cm.Parameters.AddWithValue("@cltp", CLTP);
+            cm.Parameters.AddWithValue("@cnof", Cnof);
+            cm.Parameters.AddWithValue("@ca", Ca);
+            cm.Parameters.AddWithValue("@cb", Cb);
+            cm.Parameters.AddWithValue("@cc1", Cc1);
+            cm.Parameters.AddWithValue("@cc2", Cc2);
+            cm.Parameters.AddWithValue("@cres", Cres);
+            cm.Parameters.AddWithValue("@ccd", Ccd);
+            int c = cm.ExecuteNonQuery();
+            if (c > 0)
+            {
+                res = true;
+            }
+        }
+        catch (Exception e)
+        {
+            err = e.Message;
+            res = false;
+        }
+        return res;
+    }
+    public bool UpdateCourseD()
+    {
+        bool res = false;
+        try
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Saurabh\Desktop\Appraisal\App_Data\Database.mdf;Integrated Security=True";
+            cn.Open();
+            SqlCommand cm = new SqlCommand();
+            cm.CommandText = "update CourseS set CL=@cl,CT=@ct,CP=@cp,CLTP=@cltp,cnof=@cnof,Ca=@ca,Cb=@cb,Cc1=@cc1,Cc2=@cc2,Cres=@cres,Ccd=@ccd where login=@log and term=@ter and courseid=@cid";
+            cm.Connection = cn;
+            cm.CommandType = CommandType.Text;
+            cm.Parameters.AddWithValue("@cl", CL);
+            cm.Parameters.AddWithValue("@ct", CT);
+            cm.Parameters.AddWithValue("@cp", CP);
+            cm.Parameters.AddWithValue("@cltp", CLTP);
+            cm.Parameters.AddWithValue("@cnof", Cnof);
+            cm.Parameters.AddWithValue("@ca", Ca);
+            cm.Parameters.AddWithValue("@cb", Cb);
+            cm.Parameters.AddWithValue("@cc1", Cc1);
+            cm.Parameters.AddWithValue("@cc2", Cc2);
+            cm.Parameters.AddWithValue("@cres", Cres);
+            cm.Parameters.AddWithValue("@ccd", Ccd);
+            cm.Parameters.AddWithValue("@log", log);
+            cm.Parameters.AddWithValue("@ter", Cterm);
+            cm.Parameters.AddWithValue("@cid", CID);
+            
+            int c = cm.ExecuteNonQuery();
+            if (c > 0)
+            {
+                res = true;
+            }
+        }
+        catch (Exception e)
+        {
+            err = e.Message;
+            res = false;
+        }
+        return res;
+    }
+	public CourseDetail()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
---
+++ SyllabusQ.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for SyllabusQ
+/// </summary>
+public class SyllabusQ
+{
+    private String log, remark,err;
+    private int term;
+    private decimal scerti, hcerti, pntawd;
+	public SyllabusQ()
+	{
+		
+	}
+    public String Error
+    {
+        get
+        {
+            return err;
+        }
+    }
+
+        public String Login
+    {
+        get
+        {
+            return log;
+        }
+        set
+        {
+            log = value;
+        }
+    }
+    public String Remarks
+    {
+        get
+        {
+            return remark;
+        }
+        set
+        {
+            remark = value;
+        }
+    }
+    public int Term
+    {
+        get
+        {
+            return term;
+        }
+        set
+        {
+            term = value;
+        }
+    }
+
+    public decimal SelfCerti
+    {
+        get
+        {
+            return scerti;
+        }
+        set
+        {
+            scerti = value;
+        }
+    }
+    public decimal HodCerti
+    {
+        get
+        {
+            return hcerti;
+        }
+        set
+        {
+            hcerti = value;
+        }
+    }
+    public decimal Pointsawt
+    {
+        get
+        {
+            return pntawd;
+        }
+        set
+        {
+            pntawd = value;
+        }
+    }
+    public  DataTable UserSyllabus()
+    {
+        String cns = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Saurabh\Desktop\Appraisal\App_Data\Database.mdf;Integrated Security=True";
+        String cms = "Select * from SyllabusQuality where login='" + log +"' order by Term" ;
+        SqlDataAdapter da = new SqlDataAdapter(cms, cns);
+        DataSet ds = new DataSet();
+        da.Fill(ds);
+        return ds.Tables[0];
+    }
+    public String IsEntered()
+    {
+        
+        try
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Saurabh\Desktop\Appraisal\App_Data\Database.mdf;Integrated Security=True";
+            cn.Open();
+            SqlCommand cm = new SqlCommand();
+            cm.CommandText = "select count(*) from SyllabusQuality where login=@log and term=@term";
+            cm.Connection = cn;
+            cm.CommandType = CommandType.Text;
+            cm.Parameters.AddWithValue("@log", log);
+            cm.Parameters.AddWithValue("@term", term);
+
+            int count = (int)cm.ExecuteScalar();
+            if (count > 0)
+            {
+                return "yes";
+            }
+            else
+            {
+                return "no";
+            }
+
+        }
+        catch (Exception e1)
+        {
+            err = e1.Message;
+            return "no";
+        }
+        
+    }
+    public bool NewSyllabusQ()
+    {
+        bool res = false;
+        try
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Saurabh\Desktop\Appraisal\App_Data\Database.mdf;Integrated Security=True";
+            cn.Open();
+            SqlCommand cm = new SqlCommand();
+            cm.CommandText = "insert into SyllabusQuality values(@log,@term,@selfcerti,@hodcerti,@pointsawd,@remarks)";
+            cm.Connection = cn;
+            cm.CommandType = CommandType.Text;
+            cm.Parameters.AddWithValue("@log", log);
+            cm.Parameters.AddWithValue("@term", term);
+            cm.Parameters.AddWithValue("@selfcerti", scerti);
+            cm.Parameters.AddWithValue("@hodcerti", hcerti);
+            cm.Parameters.AddWithValue("@pointsawd", pntawd);
+            cm.Parameters.AddWithValue("@remarks", remark);
+
+            int c = cm.ExecuteNonQuery();
+            if (c > 0)
+            {
+                res = true;
+            }
+        }
+        catch (Exception e)
+        {
+            err = e.Message;
+            res = false;
+        }
+        return res;
+    }
+    public bool UpdateSyllabusQ()
+    {
+        bool res = false;
+        try
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\Administrator\Documents\Visual Studio 2010\WebSites\Appraisal\App_Data\Database.mdf;Integrated Security=True;User Instance=True";
+            cn.Open();
+            SqlCommand cm = new SqlCommand();
+            cm.CommandText = "update SyllabusQuality set Selfcerti=@selfcerti,hodcerti=@hodcerti,pointsawd=@pointsawd,remarks=@remarks where login=@log and Term=@term";
+            cm.Connection = cn;
+            cm.CommandType = CommandType.Text;
+            cm.Parameters.AddWithValue("@selfcerti", scerti);
+            cm.Parameters.AddWithValue("@hodcerti", hcerti);
+            cm.Parameters.AddWithValue("@pointsawd", pntawd);
+            cm.Parameters.AddWithValue("@remarks", remark);
+            cm.Parameters.AddWithValue("@log", log);
+            cm.Parameters.AddWithValue("@term", term);
+            
+            int c = cm.ExecuteNonQuery();
+            if (c > 0)
+            {
+                res = true;
+            }
+        }
+        catch (Exception e)
+        {
+            err = e.Message;
+            res = false;
+        }
+        return res;
+    }
+
+
+}
